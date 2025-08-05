2d823f378c10df6957f563453aca96f8cfa04ad7
Georgina <georginatomass@gmail.com>
2025-08-05 19:26:17 +0200

    menu and buttons styles

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { IonIcon, IonTabButton, IonLabel, IonBadge, IonTabBar } from "@ionic/angular/standalone";
-import { heart, calendar, musicalNote, personCircleOutline, chatbubblesOutline } from 'ionicons/icons';
+import { IonIcon, IonTabButton, IonLabel, IonTabBar } from "@ionic/angular/standalone";
+import { heartOutline, homeOutline, personOutline, earthOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
-  imports: [IonTabButton, IonLabel, IonBadge, IonIcon, IonTabBar, RouterLink],
+  imports: [IonTabButton, IonLabel, IonIcon, IonTabBar, RouterLink],
 })
 export class MenuComponent  implements OnInit {
-
   constructor() { 
-    addIcons({ heart, calendar, musicalNote,personCircleOutline, chatbubblesOutline});
+    addIcons({ heartOutline, homeOutline, personOutline, earthOutline});
   }
 
   ngOnInit() {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3400/api'
+  apiUrl: 'https://ionic-cheese-back.onrender.com/api'
 };
 
 /*
