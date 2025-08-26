6cea5c86c9961f16172e69630137b1bba6769579
Georgina <georginatomass@gmail.com>
2025-08-26 16:43:38 +0200

    feat: add environment configuration management with dotenv and refactor environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,15 @@
+// src/environments/environment.template.ts
 export const environment = {
   production: true,
   apiUrl: 'https://ionic-cheese-back.onrender.com/api',
   useEmulators: false,
   firebase: {
-    apiKey: 'YOUR_API_KEY', // Cal que reemplacis amb la teva API key real
-    authDomain: 'cheese-storage.firebaseapp.com',
-    projectId: 'cheese-storage',
-    storageBucket: 'cheese-storage.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_CHEESE',
+    apiKey: 'AIzaSyB8s4uDCXbfihLyk17fIXPQyq7GEg0q4tI',
+    authDomain: 'cheese-29925.firebaseapp.com',
+    projectId: 'cheese-29925',
+    storageBucket: 'cheese-29925.firebasestorage.app',
+    messagingSenderId: '667487841882',
+    appId: '1:667487841882:web:e4cafc22a81221aa5cf263',
+    measurementId: 'G-V5G0WLWDPM',
   },
 };
